5c6e4d3f92cea8f4f172eb1ec1624b1a089fa150
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-04-29 15:43:32 +0530

    Counter app working

--- CounterApp/counter-app/src/App.jsx
+++ CounterApp/counter-app/src/App.jsx
@@ -4,6 +4,23 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick1 = () => {
+    if (counter === 0) {
+      return 0;
+    } else {
+      setCounter(counter - 1);
+    }
+  };
+  const handleClick2 = () => {
+    setCounter(counter + 1);
+  };
+  const handleClick3 = () => {
+    setCounter(0);
+    window.location.reload();
+  };
+
   return (
     <>
       <div class="app">
@@ -14,17 +31,28 @@ function App() {
               "linear-gradient(90deg, rgba(117, 42, 155, 1) 0%, rgba(87, 199, 133, 1) 50%, rgba(237, 221, 83, 1) 100%)",
           }}
         >
-        <div className="reset">
-          R
+          <div className="reset">
+            <button onClick={handleClick3} className="reset-btn">
+              R
+            </button>
+          </div>
+          <div className="count">
+            <h1> {counter}</h1>
+          </div>
         </div>
-          
+        <div className="plusminus">
+          <div className="minus switch">
+            <button onClick={handleClick1} className="minus-btn button">
+              -
+            </button>
+          </div>
+          <div className=" plus switch">
+            <button onClick={handleClick2} className="plust-btn button">
+              +
+            </button>
+          </div>
+        </div>
       </div>
-       <div className="plusminus">
-          <div className="minus switch" >-</div>
-          <div className=" plus switch">+</div>
-        </div>
-        </div>
-       
     </>
   );
 }
