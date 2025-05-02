11fcbd7ff6778a9895e8f0f8345f99bbf44fa9bf
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-02 08:34:55 +0530

    BMI app ui design

--- BMI/bmi-app/src/App.jsx
+++ BMI/bmi-app/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <div><p>Hello Bmi</p></div>
+      <div className="screen">
+        <div className="container">
+          <div className="div">BMI Calculator</div>
+          <div className="weight-text div">Weight in KG</div>
+          <div className="weight-input div input"></div>
+
+          <div className="height-text div">Weight in KG</div>
+          <div className="height-input div input"></div>
+          <div className="div">
+            <button className="button">Submit</button>
+          </div>
+          <div className="div">
+           <button className="button">Reload</button>
+          </div>
+
+          <div className="div">Your BMI is : </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
