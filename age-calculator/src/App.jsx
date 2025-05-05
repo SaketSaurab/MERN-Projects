dd042b43fd1bc31e60b1ee4e0260ee8cb9898dad
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-05 22:30:14 +0530

    UI updated

--- age-calculator/src/App.jsx
+++ age-calculator/src/App.jsx
@@ -10,9 +10,28 @@ function App() {
       <div className='container'>
         <div className='main-div'>
         <h1>Age Calculator</h1>
-        <h3>Age Calculator can calculate the age based on your Date of birth</h3>
+        <h5>Age Calculator can calculate the age based on your Date of birth</h5>
+     
+      <div className='div2'>
+      <div className='div-dob'>
+        <h3>Date of Birth</h3>
+        <input className='date-input' type='date'></input>
+      <div>
+          <button>
+        Calculate Age
+        </button>
+        <button>Reset</button>
+      </div>
+
+      </div>
+      <div className='div-output'>
+        <h3>Your age is : </h3>
+        <h2></h2>
+      </div>
+
       </div>
       </div>
+       </div>
     </>
   )
 }
