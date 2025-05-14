fc1649522cf5374dfab7f392364b3484c305ed0d
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-05-14 23:21:39 +0530

    category button added

--- restaurant-menu/src/component/basics/Restaurant.jsx
+++ restaurant-menu/src/component/basics/Restaurant.jsx
@@ -5,13 +5,19 @@ import Card from "./Card";
 
 const Restaurant = () => {
   const[menuData,setMenuData]=useState(Menu);
+  const filterItem=(category)=>{
+    const updatedList=Menu.filter((curElem)=>{
+      return curElem.category===category;
+    });
+    setMenuData(updatedList);
+  }
   return (
     <>
 <nav className="navbar">
   <div className="btn-group">
-    <button className="btn-group-item">Breakfast</button>
+    <button className="btn-group-item" onClick={()=>filterItem("breakfast")}>Breakfast</button>
     
-    <button className="btn-group-item">Lunch</button>
+    <button className="btn-group-item"onClick={()=>filterItem("lunch")}>Lunch</button>
     
     <button className="btn-group-item">Dinner</button>
   </div>
