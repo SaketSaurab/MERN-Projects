61c888c79c7016d3ee11516651dfa4b4f31239ad
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-08-18 19:35:37 +0530

    Note app created

--- simple-note-app/src/App.jsx
+++ simple-note-app/src/App.jsx
@@ -2,12 +2,43 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text,setText]=useState("");
+  const[notes,setNotes]=useState([]);
+  const addNote=()=>{
+    if(!text.trim()) return;
+    setNotes([...notes,{id:Date.now(),text}]);
+    setText("");
+  }
+
+  const deleteNote=()=>{
+    setNotes(notes.filter((note)=>note.id !==id));
+  };
+
 
   return (
     <>
-     <div>
-     Hello World
+     <div className='main-div'>
+     <div id='app-title'>Note Taking App</div>
+     <textarea className='text-area' value={text} onChange={(e)=>{
+      setText(e.target.value);
+     }
+     }
+
+      placeholder='Write note ....'
+     />
+     <div className='button'>
+      <button onClick={addNote}>Add</button>
+      <button onClick={deleteNote}>Delete</button>
+
+     {notes.map((note)=>(
+      <div key={note.id}>
+      <p>{note.text}</p>
+      <button onClick={()=>deleteNote(note.id)}>Delete</button>
+      </div>
+     ))}
+
+
+     </div>
      </div>
     </>
   )
