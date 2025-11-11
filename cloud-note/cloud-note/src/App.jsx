96715d7e0b375834596cc21ed369536d2a93e341
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-11-11 17:56:09 +0530

    tailwind added

---
+++ cloud-note/cloud-note/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-red-500 text-black">
+      <h1 className="text-4xl font-bold">Tailwind added</h1>
+    </div>
+  );
+}
+
+export default App;
