049c4a9938b197828be25a9810dc6bb37009d89e
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-11-15 18:48:30 +0530

    Pop-up feature added

--- cloud-note/cloud-note/src/App.jsx
+++ cloud-note/cloud-note/src/App.jsx
@@ -8,16 +8,29 @@ function App() {
     return savedNotes ? JSON.parse(savedNotes) : [];
   });
   const [editId, setEditId] = useState(null);
+  const [selectedNote, setSelectedNote] = useState(null);
 
   const addNote = () => {
     if (!title || !content) return;
 
-    const newNote = {
-      id: Date.now(),
-      title: title,
-      content: content,
-    };
-    setNotes([...notes, newNote]);
+    {
+      /* updating the note */
+    }
+    if (editId !== null) {
+      const updatedNotes = notes.map((note) =>
+        note.id === editId ? { ...note, title: title, content: content } : note,
+      );
+      setNotes(updatedNotes);
+      setEditId(null);
+    } else {
+      const newNote = {
+        id: Date.now(),
+        title: title,
+        content: content,
+      };
+      setNotes([...notes, newNote]);
+    }
+
     setTitle("");
     setContent("");
   };
@@ -35,9 +48,13 @@ function App() {
     setContent(note.content);
     setEditId(note.id);
   };
+
+  {
+    /* new state for pop-up */
+  }
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Note App</h1>
+      <h1 className="text-2xl font-bold mb-4 ">Note App</h1>
       <div className="bg-white p-4 rounded shadow-md">
         <input
           className="w-full border rounded-md p-2 mb-2 "
@@ -54,16 +71,17 @@ function App() {
         />
         <button
           onClick={addNote}
-          className="bg-blue-500 text-white py-2 px-4 rounded"
+          className={` text-white py-2 px-4 rounded  ${editId !== null ? "bg-green-500 hover:bg-green-600" : "bg-blue-500 hover:bg-blue-600"}`}
         >
-          Add Note
+          {editId !== null ? "Update Note" : " Add Note"}
         </button>
       </div>
-      <div className="flex flex-wrap gap-4 mt-6">
+      <div className="flex flex-wrap gap-4 mt-6 ">
         {notes.map((note) => (
           <div
             key={note.id}
-            className="w-48 h-48 bg-white border rounded shadow p-3"
+            onClick={() => setSelectedNote(note)}
+            className="w-48 h-48 bg-white border rounded-xl shadow p-3 hover:shadow-xl transition cursor-pointer"
           >
             <h2 className="font-semibold text-lg mb-2 truncate ">
               {note.title}
@@ -72,20 +90,42 @@ function App() {
               {note.content}
             </p>
             <button
-              onClick={() => deleteNote(note.id)}
-              className="bg-red-500 text-sm mt-2 p-1 rounded text-white"
+              onClick={(e) => {
+                e.stopPropagation();
+                deleteNote(note.id);
+              }}
+              className="bg-red-500 text-sm mt-2 p-1 rounded text-white hover:bg-red-600"
             >
               Delete
             </button>
             <button
-              onClick={() => startEditing(note)}
-              className="text-sm bg-green-400 p-1 text-white ml-2 rounded"
+              onClick={(e) => {
+                e.stopPropagation();
+                startEditing(note);
+              }}
+              className="text-sm bg-green-400 p-1 hover:bg-green-600 text-white ml-2 rounded"
             >
               Edit
             </button>
           </div>
         ))}
-      </div>
+      </div>{" "}
+      {selectedNote && (
+        <div className="fixed inset-0 bg-gray-500/30 backdrop-blur flex justify-center items-center ">
+          <div className="bg-white p-6 rounded-lg max-w-[70vw] max-h-[70vh] overflow-y-auto">
+            <h2 className="text-xl font-bold mb-2">{selectedNote.title}</h2>
+            <p className="text-gray-700 whitespace-pre-wrap ">
+              {selectedNote.content}
+            </p>
+            <button
+              className="mt-4 bg-blue-500 hover:bg-blue-600 rounded text-white px-4 py-2"
+              onClick={() => setSelectedNote(null)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
