e683dc85ee683b3c0b2a54db9fc3048af4958072
Saket Saurabh <100rabhsinghalld@gmail.com>
2025-11-23 19:50:32 +0530

    go to trash and go to notes button issuse fixed

--- NoteSphere/NoteSphere/src/pages/Home.jsx
+++ NoteSphere/NoteSphere/src/pages/Home.jsx
@@ -2,20 +2,28 @@ import React, { useState } from "react";
 import NotesPage from "../components/notes/NotesPage";
 
 const Home = () => {
-  const [mode, setMode] = useState(null);
+  const [mode, setMode] = useState(() => {
+    return localStorage.getItem("mode") || null;
+  });
   if (!mode) {
     return (
       <div className="flex flex-col items-center justify-center h-screen gap-6">
         <h1 className=" text-3xl font-bold">Choose</h1>
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded w-48 hover:bg-blue-600"
-          onClick={() => setMode("offline")}
+          onClick={() => {
+            localStorage.setItem("mode", "offline");
+            setMode("offline");
+          }}
         >
           Continue Offline
         </button>
         <button
           className="bg-green-500 text-white px-4 py-2 w-48 rounded hover:bg-green-600"
-          onClick={() => setMode("online")}
+          onClick={() => {
+            localStorage.setItem("mode", "online");
+            setMode("online");
+          }}
         >
           Use Cloud (Login)
         </button>
